c2cc6c7e95be92cb81b6a50d6c9a156bd02f5e1b
Bogdan Floris <bogdan.floris@gmail.com>
2026-03-28 21:18:20 +0200

    feat: add a 8086 helper file copying some tables from the user manual

--- src/sim8086/decoder.hpp
+++ src/sim8086/decoder.hpp
@@ -12,6 +12,8 @@ enum Reg : uint8_t {
     bx,
 };
 
+// Instruction decoding for the 8086 instruction set.
+// Please see ./doc/8086.txt for the reference.
 struct Instruction {
     Op op;
     Reg src;
