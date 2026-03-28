5cdc0ec784fbb9f1007cf62d34991d1c29b0cebc
Bogdan Floris <bogdan.floris@gmail.com>
2026-03-28 17:52:59 +0200

    fix: switch regs

--- src/sim8086/decoder.cpp
+++ src/sim8086/decoder.cpp
@@ -3,7 +3,7 @@
 Instruction decode(const std::vector<uint8_t>& bytes) {
     return Instruction{
         .op = Op::mov,
-        .src = Reg::ax,
-        .dst = Reg::bx,
+        .src = Reg::bx,
+        .dst = Reg::ax,
     };
 }
--- src/sim8086/tests/decoder_test.cpp
+++ src/sim8086/tests/decoder_test.cpp
@@ -7,6 +7,6 @@ TEST(Decoder, MovRegToReg) {
     std::vector<uint8_t> bytes = {0x89, 0xd8};
     auto instr = decode(bytes);
     EXPECT_EQ(instr.op, Op::mov);
-    EXPECT_EQ(instr.src, Reg::ax);
-    EXPECT_EQ(instr.dst, Reg::bx);
+    EXPECT_EQ(instr.src, Reg::bx);
+    EXPECT_EQ(instr.dst, Reg::ax);
 }
